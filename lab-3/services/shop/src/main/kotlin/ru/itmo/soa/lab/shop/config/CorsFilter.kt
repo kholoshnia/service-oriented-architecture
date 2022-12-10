0243a3927ba58fb67c8420aaa453241ab3b3e73b
Vadim Kholoshnia <vdkholoshnia@gmail.com>
2022-12-10 23:11:59 +0500

    Update lab-3

--- lab-3/services/shop/src/main/kotlin/ru/itmo/soa/lab/shop/config/CorsFilter.kt
+++ lab-3/services/shop/src/main/kotlin/ru/itmo/soa/lab/shop/config/CorsFilter.kt
@@ -16,25 +16,16 @@ class CorsFilter: Filter {
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val request = req as HttpServletRequest
         val response = res as HttpServletResponse
+
         response.setHeader("Access-control-Allow-Origin", "*")
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token")
+        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, PATCH, DELETE")
+        response.setHeader("Access-Control-Allow-Headers", "*")
         response.setHeader("Access-Control-Max-Age", "3600")
         response.setHeader("Access-Control-Allow-Credentials", "true")
+
         if (!request.method.equals("OPTIONS", ignoreCase = true)) {
-            try {
-                chain.doFilter(req, res)
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-            }
+            chain.doFilter(request, response)
         } else {
-            println("Pre-flight")
-            response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
-            response.setHeader("Access-Control-Max-Age", "3600")
-            response.setHeader(
-                "Access-Control-Allow-Headers", "authorization, content-type,x-auth-token, " +
-                        "access-control-request-headers, access-control-request-method, accept, origin, authorization, x-requested-with"
-            )
             response.status = HttpServletResponse.SC_OK
         }
     }
