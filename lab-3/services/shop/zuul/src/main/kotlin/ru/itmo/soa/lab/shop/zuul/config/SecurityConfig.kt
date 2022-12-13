cca4899ad6b91cf3b5152a60c27cc12475437be2
Vadim Kholoshnia <vdkholoshnia@gmail.com>
2022-12-13 07:14:25 +0500

    Update lab-3

--- lab-3/services/shop/zuul/src/main/kotlin/ru/itmo/soa/lab/shop/zuul/config/SecurityConfig.kt
+++ lab-3/services/shop/zuul/src/main/kotlin/ru/itmo/soa/lab/shop/zuul/config/SecurityConfig.kt
@@ -1,21 +1,9 @@
 package ru.itmo.soa.lab.shop.zuul.config
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory
-import org.apache.http.conn.ssl.TrustStrategy
-import org.apache.http.impl.client.HttpClients
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.client.RestTemplate
-import java.security.KeyManagementException
-import java.security.KeyStoreException
-import java.security.NoSuchAlgorithmException
-import java.security.cert.X509Certificate
-
 
 @Configuration
 @EnableWebSecurity
@@ -23,28 +11,5 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll();
         http.requiresChannel().anyRequest().requiresSecure()
-    }
-}
-
-@Configuration
-class SslConfiguration() {
-    @Bean
-    @Throws(
-        KeyStoreException::class,
-        NoSuchAlgorithmException::class,
-        KeyManagementException::class
-    )
-    fun restTemplate(): RestTemplate? {
-        val acceptingTrustStrategy =
-            TrustStrategy { x509Certificates: Array<X509Certificate?>?, s: String? -> true }
-        val sslContext =
-            org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build()
-        val csf =
-            SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier())
-        val httpClient =
-            HttpClients.custom().setSSLSocketFactory(csf).build()
-        val requestFactory = HttpComponentsClientHttpRequestFactory()
-        requestFactory.httpClient = httpClient
-        return RestTemplate(requestFactory)
     }
 }
